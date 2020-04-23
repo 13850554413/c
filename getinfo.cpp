0e25829a1c42c8c619657b467f5ff2a4f5a5bf22
yihui <hu.yihui@163.com>
2020-04-23 22:57:55 +0800

    新增 c++ primer plus 教材示例“

---
+++ getinfo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int carrots;
+
+    cout << "how many carrots do you have?" << endl;
+    cin >> carrots;
+    cout << "here are two more. ";
+    carrots = carrots + 2;
+    cout << "now you have" << carrots << "carrots." << endl;
+    return 0;
+}
